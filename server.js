5bacfe7c98324547325042f298135ab9ec00f8b4
Sirine AZZOUZI <95279874+azzouzisirin@users.noreply.github.com>
2023-07-13 12:59:06 +0100

    Update and rename app.js to server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const port=6000
+
+
+
+server.listen(port, () => { 
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
